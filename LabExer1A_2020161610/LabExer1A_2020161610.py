de20ad3a8a372d00eb3c15fbd6d4653585ea5c04
JvMapote <jayveenmapote@gmail.com>
2022-04-16 00:03:57 +0800

    commit

--- LabExer1A_2020161610/LabExer1A_2020161610.py
+++ LabExer1A_2020161610/LabExer1A_2020161610.py
@@ -1,38 +1,46 @@
-class Node:
-    # Singly linked node
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-class singly_linked_list:
-    def __init__(self):
-        # Createe an empty list
-        self.head = None
-        self.tail = None
-        self.count = 0
-    def iterate_item(self):
-        # Iterate the list.
-        current_item = self.head
-        while current_item:
-            val = current_item.data
-            current_item = current_item.next
-            yield val
-    def append_item(self, data):
-        #Append items on the list
-        node = Node(data)
-        if self.tail:
-            self.tail.next = node
-            self.tail = node
-        else:
-            self.head = node
-            self.tail = node
-        self.count += 1
-items = singly_linked_list()
-items.append_item('PHP')
-items.append_item('Python')
-items.append_item('C#')
-items.append_item('C++')
-items.append_item('Java')
-for val in items.iterate_item():
-    print(val)
-print("\nhead.data: ",items.head.data)
-print("tail.data: ",items.tail.data)
+  
+class Node:    
+    def __init__(self,data):    
+        self.data = data;    
+        self.previous = None;    
+        self.next = None;    
+class SearchList:      
+    def __init__(self):    
+        self.head = None;    
+        self.tail = None;      
+    def addNode(self, data):      
+        newNode = Node(data);    
+        if(self.head == None):      
+            self.head = self.tail = newNode;     
+            self.head.previous = None;      
+            self.tail.next = None;    
+        else:       
+            self.tail.next = newNode;       
+            newNode.previous = self.tail;      
+            self.tail = newNode;        
+            self.tail.next = None;     
+    def searchNode(self, value):    
+        i = 1;    
+        flag = False;      
+        current = self.head;        
+        if(self.head == None):    
+            print("List is empty");    
+            return;    
+        while(current != None):      
+            if(current.data == value):    
+                flag = True;    
+                break;    
+            current = current.next;    
+            i = i + 1;    
+        if(flag):
+            print(True)      
+        else:  
+            print(False)   
+dList = SearchList();     
+dList.addNode(1);    
+dList.addNode(5);    
+dList.addNode(4);    
+dList.addNode(2);    
+dList.addNode(3);    
+dList.searchNode(4);      
+dList.searchNode(5);
